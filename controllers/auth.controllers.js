bb56cee7e63d975aec453d7d8f018303bf86598e
webformulator <mddalam1@gmail.com>
2022-08-28 18:40:44 +0530

    Created and Sent Refresh Token.

--- controllers/auth.controllers.js
+++ controllers/auth.controllers.js
@@ -8,10 +8,16 @@ const login = async (req, res) => {
       (user) => user.username === username && user.password === password
     );
     if (!user) return res.status(400).json({ message: "Invalid credentials" });
-    const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
       expiresIn: "18000s",
     });
-    return res.status(200).json({ data: token, message: "Login successfully" });
+    const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET, {
+      expiresIn: "1y",
+    });
+    return res.status(200).json({
+      data: { accessToken, refreshToken },
+      message: "Login successfully",
+    });
   } catch (error) {
     console.log(error);
     return res.status(500).json({ message: "Something went wrong" });
